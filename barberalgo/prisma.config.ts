e029d1a6cfb6f396bcd4febb8e768bc87d6e5760
Gabriel André <gabrielandre699@gmail.com>
2026-06-09 20:36:54 -0300

    fix: fixed prisma config settings

--- barberalgo/prisma.config.ts
+++ barberalgo/prisma.config.ts
@@ -1,7 +1,7 @@
 /** @format */
 
 import "dotenv/config";
-import { defineConfig } from "prisma/config";
+import { defineConfig, env } from "prisma/config";
 
 export default defineConfig({
   schema: "prisma/schema.prisma",
@@ -9,6 +9,6 @@ export default defineConfig({
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: env("DATABASE_URL"),
   },
 });
